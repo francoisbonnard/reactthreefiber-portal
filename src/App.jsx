0f4fa1f832d1897a7a1f160d42748213b6c79bc9
francoisbonnard <francois.bonnard@gmail.com>
2023-08-05 16:20:08 +0200

    3 textures and a sphere

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Canvas } from "@react-three/fiber"
+import { Experience } from "./components/Experience"
+import React, { Suspense } from "react"
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas
+      shadows
+      camera={{ position: [3, 3, 3], fov: 30 }}
+    >
+      <color
+        attach='background'
+        args={["#ececec"]}
+      />
+      {/* <React.Suspense fallback={<></>}> */}
       <Experience />
+      {/* </React.Suspense> */}
     </Canvas>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, useTexture, OrbitControls } from "@react-three/drei"
 
 export const Experience = () => {
+  const myMap = useTexture("textures/radiant_rocks_in_thje_jungle.jpg")
+
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <Environment preset='sunset' />
       <OrbitControls />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial map={myMap} />
       </mesh>
     </>
-  );
-};
+  )
+}
