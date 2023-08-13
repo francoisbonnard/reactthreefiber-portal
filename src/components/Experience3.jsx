861c6cf76bd6acd47ff2c0a4701828672b442c2f
francois bonnard <francois.bonnard@gmail.com>
2023-08-13 23:46:10 +0200

    utilisation de forwardRef pour propager une référence sur ecrin_bas

--- src/components/Experience3.jsx
+++ src/components/Experience3.jsx
@@ -6,38 +6,38 @@ import {
   RoundedBox,
   Text,
   CameraControls,
-} from "@react-three/drei"
+} from "@react-three/drei";
 
-import { useFrame, useThree } from "@react-three/fiber"
-import * as easing from "maath/easing"
-import React, { useState, useRef, useEffect } from "react"
-import * as THREE from "three"
+import { useFrame, useThree } from "@react-three/fiber";
+import * as easing from "maath/easing";
+import React, { useState, useRef, useEffect } from "react";
+import * as THREE from "three";
 // import { BackSide } from "three"
 
-import { Stars } from "./Stars"
-import { A_ecrin_bas3 } from "./A_ecrin_bas3"
-import { A_ecrin_haut } from "./A_ecrin_haut"
-import { A_diamant } from "./A_diamant"
+import { Stars } from "./Stars";
+import { A_ecrin_bas3 } from "./A_ecrin_bas3";
+import { A_ecrin_haut } from "./A_ecrin_haut";
+import { A_diamant } from "./A_diamant";
 
 export const Experience3 = () => {
-  const [active, setActive] = useState(null)
-  const [hovered, setHovered] = useState(null)
+  const [active, setActive] = useState(null);
+  const [hovered, setHovered] = useState(null);
 
   const [meshInfos, setMeshInfos] = useState({
     size1: null,
     rotations1: null,
-  })
+  });
 
   // animate the camera
-  const { camera } = useThree()
-  const scene = useThree((state) => state.scene)
-  const controlsRef = useRef()
+  const { camera } = useThree();
+  const scene = useThree((state) => state.scene);
+  const controlsRef = useRef();
 
   useEffect(() => {
-    camera.updateProjectionMatrix()
+    camera.updateProjectionMatrix();
     if (active) {
-      const targetPosition = new THREE.Vector3()
-      scene.getObjectByName(active).getWorldPosition(targetPosition)
+      const targetPosition = new THREE.Vector3();
+      scene.getObjectByName(active).getWorldPosition(targetPosition);
       controlsRef.current.setLookAt(
         0,
         0,
@@ -46,76 +46,92 @@ export const Experience3 = () => {
         targetPosition.y,
         targetPosition.z,
         true
-      )
+      );
     } else {
-      controlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true)
+      controlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true);
     }
-  }, [active])
+  }, [active]);
 
+  
+  // animate the rotation
+  const meshRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+  
+  useFrame(() => {
+    if (isHovered ) 
+  {   console.log("meshRef.current value:", meshRef.current);}
+    // console.log("isHovered value:", isHovered);
+    if (isHovered && meshRef.current) {
+    console.log("hovered");
+      meshRef.current.rotation.y += 0.05;
+    }
+  });
+  
+  useEffect(() => {
+    console.log(meshRef.current);
+  }, []);
+  
   return (
     <>
       <ambientLight intensity={0.01} />
       <CameraControls ref={controlsRef} />
 
       <EcrinStage
-        name='Ecrin'
-        color='#381f14'
+        name="Ecrin"
+        color="#381f14"
         ambiantIntensity={0}
         environmentPreset={"dawn"}
-        texture='./textures/radiant_rocks_in_thje_jungle.jpg'
+        texture="./textures/radiant_rocks_in_thje_jungle.jpg"
         position-z={-0.5}
-        active={active}
-        setActive={setActive}
         meshInfos={meshInfos}
-      >
-        <A_ecrin_bas3
-          scale={0.6}
-          position-y={-1}
+        >
+        <A_ecrin_bas3 scale={0.6} position-y={-1}
+        ref={meshRef}
+        onPointerOver={() => {
+          setIsHovered(true);
+          console.log("Mouse Over");
+        }}
+        onPointerOut={() => {
+          setIsHovered(false);
+          console.log("Mouse Out");
+        }}
         />
         <A_ecrin_haut
-          hovered={hovered}
-          setHovered={setHovered}
           scale={0.6}
           position-y={-1}
         />
       </EcrinStage>
 
       <PortalStage
-        name='Diamant'
-        color='#4c3d39'
+        name="Diamant"
+        color="#4c3d39"
         ambiantIntensity={0.1}
         environmentPreset={"night"}
-        texture='./textures/sky_an_horizon_with_clouds.jpg'
+        texture="./textures/sky_an_horizon_with_clouds.jpg"
         position-x={-2.5}
         rotation-y={Math.PI / 8}
         active={active}
         setActive={setActive}
       >
-        <A_diamant
-          scale={1}
-          position-y={-1}
-        />
+        <A_diamant scale={1} position-y={-1} />
       </PortalStage>
 
       <PortalStage
-        name='Stars'
-        color='#2b2744'
+        name="Stars"
+        color="#2b2744"
         ambiantIntensity={0.1}
         environmentPreset={"dawn"}
-        texture='./textures/surreal_scary_mountains.jpg'
+        texture="./textures/surreal_scary_mountains.jpg"
         position-x={2.5}
         rotation-y={-Math.PI / 8}
         active={active}
         setActive={setActive}
       >
-        <Stars
-          scale={1}
-          position-y={-0.5}
-        />
+        <Stars scale={1} position-y={-0.5} />
       </PortalStage>
     </>
-  )
-}
+  );
+};
 
 const PortalStage = ({
   children,
@@ -128,12 +144,18 @@ const PortalStage = ({
   setActive,
   ...props
 }) => {
-  const myMap = useTexture(texture)
-  const portalMaterial = useRef()
+  const myMap = useTexture(texture);
+  const portalMaterial = useRef();
   useFrame((_state, delta) => {
-    const worldOpen = active === name
-    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.25, delta)
-  })
+    const worldOpen = active === name;
+    easing.damp(
+      portalMaterial.current,
+      "blend",
+      worldOpen ? 1 : 0,
+      0.25,
+      delta
+    );
+  });
   return (
     <group {...props}>
       <RoundedBox
@@ -142,16 +164,13 @@ const PortalStage = ({
         onDoubleClick={() => setActive(active === name ? null : name)}
       >
         <Text
-          font='fonts/Caprasimo-Regular.ttf'
+          font="fonts/Caprasimo-Regular.ttf"
           fontSize={0.3}
           position={[0, -1.3, 0.051]}
           anchorY={"bottom"}
         >
           {name}
-          <meshBasicMaterial
-            color={color}
-            toneMapped={false}
-          />
+          <meshBasicMaterial color={color} toneMapped={false} />
         </Text>
         <MeshPortalMaterial
           ref={portalMaterial}
@@ -165,16 +184,15 @@ const PortalStage = ({
           <mesh>
             <sphereGeometry args={[5, 32, 32]} />
 
-            <meshStandardMaterial
-              map={myMap}
-              side={THREE.BackSide}
-            />
+            <meshStandardMaterial map={myMap} side={THREE.BackSide} />
           </mesh>
         </MeshPortalMaterial>
       </RoundedBox>
     </group>
-  )
-}
+  );
+};
+
+
 
 const EcrinStage = ({
   children,
@@ -193,17 +211,14 @@ const EcrinStage = ({
       <Environment preset={environmentPreset} />
       {children}
       <Text
-        font='fonts/Caprasimo-Regular.ttf'
+        font="fonts/Caprasimo-Regular.ttf"
         fontSize={0.3}
         position={[0, -1.3, 0.051]}
         anchorY={"bottom"}
       >
         {name}
-        <meshBasicMaterial
-          color={color}
-          toneMapped={false}
-        />
+        <meshBasicMaterial color={color} toneMapped={false} />
       </Text>
     </group>
-  )
-}
+  );
+};
